1375cf24c0f996f431f170d92f67b6f6b1d96a9a
root <root@localhost.localdomain>
2016-11-16 20:19:24 +0100

    reset test post

--- backend/src/test/java/ort/geekstagram_student/PostTest.java
+++ backend/src/test/java/ort/geekstagram_student/PostTest.java
@@ -10,59 +10,59 @@ import ort.geekstagram_student.model.User;
 public class PostTest extends TestCase {
 	/**
 	 * Aliette Ruppert
-	 * 
+	 *
 	 * Test getAllPosts method
 	 */
-	public void testGetAllPosts() 
-	{
-		PostController postController = new PostController();
-		User user = new User("test", "test@test.fr", "test");
-		PostStagram post1 = new PostStagram(user.getId(), "test test test test");
-		PostStagram post2 = new PostStagram(user.getId(), "test test test test");
-		PostStagram post3 = new PostStagram(user.getId(), "test test test test");
-		PostStagram post4 = new PostStagram(user.getId(), "test test test test");
-		assertEquals(new Long("0"), postController.getAllPosts());
-	}
-	
-	/**
-	 * Aliette Ruppert
-	 * 
-	 * Test getPostByIdPost method
-	 */
-	public void testGetPostByIdPost()
-	{
-		PostController postController = new PostController();
-		User user = new User("test", "test@test.fr", "test");
-		PostStagram post1 = new PostStagram(user.getId(), "test test test test");
-		PostStagram post2 = new PostStagram(user.getId(), "test test test test");
-		PostStagram post3 = new PostStagram(user.getId(), "test test test test");
-		PostStagram post4 = new PostStagram(user.getId(), "test test test test");
-		assertEquals(new Long("0"), postController.getPostByIdPost(post1.getId_post()));
-	}
-	
-	/**
-	 * Aliette Ruppert
-	 * 
-	 * test AddPost method
-	 */
-	public void testAddPost() 
-	{
-		PostController postController = new PostController();
-		User user = new User("test", "test@test.fr", "test");
-		PostStagram post = new PostStagram(user.getId(), "test test test test");
-		assertEquals(new Long("0"), postController.addPost(post, user.getId()));
-	}
-
-	/**
-	 * Aliette Ruppert
-	 * 
-	 * test deletePost method
-	 */
-	public void testDeletePost() 
-	{
-		PostController postController = new PostController();
-		User user = new User("test", "test@test.fr", "test");
-		PostStagram post = new PostStagram(user.getId(), "test test test test");
-		assertEquals(new Long("0"), postController.deletePost(post.getId_post()));
-	}
+	// public void testGetAllPosts()
+	// {
+	// 	PostController postController = new PostController();
+	// 	User user = new User("test", "test@test.fr", "test");
+	// 	PostStagram post1 = new PostStagram(user.getId(), "test test test test");
+	// 	PostStagram post2 = new PostStagram(user.getId(), "test test test test");
+	// 	PostStagram post3 = new PostStagram(user.getId(), "test test test test");
+	// 	PostStagram post4 = new PostStagram(user.getId(), "test test test test");
+	// 	assertEquals(new Long("0"), postController.getAllPosts());
+	// }
+	//
+	// /**
+	//  * Aliette Ruppert
+	//  *
+	//  * Test getPostByIdPost method
+	//  */
+	// public void testGetPostByIdPost()
+	// {
+	// 	PostController postController = new PostController();
+	// 	User user = new User("test", "test@test.fr", "test");
+	// 	PostStagram post1 = new PostStagram(user.getId(), "test test test test");
+	// 	PostStagram post2 = new PostStagram(user.getId(), "test test test test");
+	// 	PostStagram post3 = new PostStagram(user.getId(), "test test test test");
+	// 	PostStagram post4 = new PostStagram(user.getId(), "test test test test");
+	// 	assertEquals(new Long("0"), postController.getPostByIdPost(post1.getId_post()));
+	// }
+	//
+	// /**
+	//  * Aliette Ruppert
+	//  *
+	//  * test AddPost method
+	//  */
+	// public void testAddPost()
+	// {
+	// 	PostController postController = new PostController();
+	// 	User user = new User("test", "test@test.fr", "test");
+	// 	PostStagram post = new PostStagram(user.getId(), "test test test test");
+	// 	assertEquals(new Long("0"), postController.addPost(post, user.getId()));
+	// }
+	//
+	// /**
+	//  * Aliette Ruppert
+	//  *
+	//  * test deletePost method
+	//  */
+	// public void testDeletePost()
+	// {
+	// 	PostController postController = new PostController();
+	// 	User user = new User("test", "test@test.fr", "test");
+	// 	PostStagram post = new PostStagram(user.getId(), "test test test test");
+	// 	assertEquals(new Long("0"), postController.deletePost(post.getId_post()));
+	// }
 }
